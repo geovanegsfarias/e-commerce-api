b347e63e1f1505de4502f5b4468efdfdda6493c1
Geovane <geovanegsf02@gmail.com>
2026-03-13 16:21:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# API de E-Commerce
+![Java](https://img.shields.io/badge/Java-21-E67E22)
+![Spring Boot](https://img.shields.io/badge/Spring_Boot-3.5.11-85EA2D)
---
+++ src/main/java/com/geovane/e_commerce_api/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.geovane.e_commerce_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
